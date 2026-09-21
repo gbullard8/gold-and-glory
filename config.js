0301402c70fd6f3b8fee6da3eb74d2af93bfbf4a
gbullard8 <gbullard8@yahoo.com>
2026-09-21 16:26:16 -0700

    Initial Gold and Glory app setup

---
+++ config.js
@@ -0,0 +1,6 @@
+// Fill these in after creating the Supabase project.
+// The publishable/anon key is intended for browser use. Never put a service-role key here.
+window.GG_CONFIG = {
+  supabaseUrl: "PASTE_SUPABASE_PROJECT_URL_HERE",
+  supabaseAnonKey: "PASTE_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE"
+};
